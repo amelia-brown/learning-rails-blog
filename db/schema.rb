4e1f85f11ae1bd99c71804a4e068549527ebb778
Amelia Brown <amelia.brown389@gmail.com>
2016-02-05 00:35:06 -0500

    Make a basic User model (including secure passwords)

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204191602) do
+ActiveRecord::Schema.define(version: 20160205052552) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",        null: false
-    t.datetime "updated_at",        null: false
-    t.string   "password_digested"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "password_digest"
   end
 
