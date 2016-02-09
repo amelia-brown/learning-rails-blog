a2e0029d257ef3706401f4b0313022751657fc59
Amelia Brown <amelia.brown389@gmail.com>
2016-02-09 15:26:01 -0500

    Finish log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,8 +13,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                             password_confirmation: "bar" }
   end
   assert_template 'users/new'
-  assert_select 'div#<CSS id for error explanation>'
-  assert_select 'div.<CSS class for field with error>'
+  assert_select 'div#error_explanation'
+  assert_select 'div.alert.alert-danger'
   end
 
   test "valid signup information" do
@@ -26,5 +26,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password"}
     end
     assert_template 'users/show'
+    assert is_logged_in?
+    assert_not flash.empty?
   end
 end
